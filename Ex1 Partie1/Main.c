f7b4776e8ac67e60a8c434981a09a8e7a049a45a
gaetandallencourt-ui <gaetandallencourt2007@gmail.com>
2026-09-09 14:23:35 +0200

    Ajoutez des fichiers projet.

---
+++ Ex1 Partie1/Main.c
@@ -0,0 +1,9 @@
+// Programme ValeurAbsolue
+let V = parseInt(prompt("Saisir un nombre entier : "));
+
+if (V >= 0) {
+	console.log(V);
+}
+else {
+	console.log(-V);
+}
---
+++ Ex1/Main.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main() {
+    int tableau[2][10];
+    int i;
+
+    // Remplissage du tableau
+    for (i = 0; i < 10; i++) {
+        tableau[0][i] = i;          // 1ère ligne : 0 à 9
+        tableau[1][i] = i * 3;     // 2ème ligne : triple des valeurs
+    }
+
+    // Affichage du tableau
+    printf("Ligne 1 : ");
+    for (i = 0; i < 10; i++) {
+        printf("%d ", tableau[0][i]);
+    }
+
+    printf("\nLigne 2 : ");
+    for (i = 0; i < 10; i++) {
+        printf("%d ", tableau[1][i]);
+    }
+
+    return 0;
+}
